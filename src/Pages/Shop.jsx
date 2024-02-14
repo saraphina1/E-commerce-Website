aa19f51e458e2536f9d778f57a28cfc70adafffd
saraphina1 <oyeyemibunmi21@gmail.com>
2024-02-14 13:56:10 +0100

    removed Newsletter

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,7 +1,6 @@
 
 import Hero from "../Components/Hero"
 import NewCollections from "../Components/NewCollections"
-import NewsLetter from "../Components/NewsLetter"
 import Popular from "../Components/Popular"
 import Offers from "../Components/offers"
 
@@ -13,7 +12,7 @@ const Shop = () => {
       <Popular/>
       <Offers/>
       <NewCollections/>
-      <NewsLetter/>
+      {/* <NewsLetter/> */}
       
     </div>
   )
